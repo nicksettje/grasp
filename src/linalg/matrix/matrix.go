6fabd39ce5ae627e2ed50b06a9dcfff4c5a60e2a
Nick <nicksettje@gmail.com>
2016-08-27 10:30:20 +0000

    Matrix doc

--- src/linalg/matrix/matrix.go
+++ src/linalg/matrix/matrix.go
@@ -1,3 +1,6 @@
+// Package matrix provides wrappers for matrix functionality.
+// The goal is to have a single consistent interface for ML code.
+// If you want to change the matrix backend, simply write new wrappers for your matrix library.
 package matrix
 
 func matrix() {
